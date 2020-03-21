b96d14e2a391709c65930dac3df90c766a600f78
Florian Fusseder <fussederflorian@gmail.com>
2020-03-21 18:42:22 +0100

    First commit

---
+++ src/main/java/ff/sprint/helper/frontend/controller/IndexController.java
@@ -0,0 +1,10 @@
+package ff.sprint.helper.frontend.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+
+}
+
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,56 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    } else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    var socket = new SockJS('http://localhost:8081/websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/text_message', function (message) {
+            console.log(JSON.parse(message.body));
+            showGreeting(JSON.parse(message.body).message);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendName() {
+    stompClient.send("/app/text_message", {}, JSON.stringify({'message': $("#message").val()}));
+}
+
+function showGreeting(message) {
+    $("#messages").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $("form").on('submit', function (e) {
+        e.preventDefault();
+    });
+    $("#connect").click(function () {
+        connect();
+    });
+    $("#disconnect").click(function () {
+        disconnect();
+    });
+    $("#send").click(function () {
+        sendName();
+    });
+});
