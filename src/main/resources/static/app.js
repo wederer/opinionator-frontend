06fdd95cc8e87c740269ebed55f574feeba35964
Florian Fusseder <fussederflorian@gmail.com>
2020-03-22 17:18:21 +0100

    Added UserCounter

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -17,27 +17,39 @@ function connect() {
     stompClient.connect({}, function (frame) {
         setConnected(true);
         console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/user_count', function (user_count) {
+            console.log(JSON.parse(user_count.body));
+            setUsers(JSON.parse(user_count.body).userCount);
+        });
         stompClient.subscribe('/topic/text_message', function (message) {
             console.log(JSON.parse(message.body));
             showGreeting(JSON.parse(message.body).message);
         });
+
     });
 }
 
 function disconnect() {
     if (stompClient !== null) {
+        stompClient.unsubscribe('/topic/text_message');
         stompClient.disconnect();
     }
     setConnected(false);
     console.log("Disconnected");
 }
 
-function sendName() {
+function sendMessage() {
     stompClient.send("/app/text_message", {}, JSON.stringify({'message': $("#message").val()}));
 }
 
 function showGreeting(message) {
     $("#messages").append("<tr><td>" + message + "</td></tr>");
+    $("#user_count").text('6')
+
+}
+
+function setUsers(user_count) {
+    $("#user_count").text(user_count)
 }
 
 $(function () {
@@ -51,6 +63,6 @@ $(function () {
         disconnect();
     });
     $("#send").click(function () {
-        sendName();
+        sendMessage();
     });
 });
